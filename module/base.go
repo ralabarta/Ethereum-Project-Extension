bac07dfa489f3fa78c4fee5282e5bcb3a12f66b1
Rubiel Alejandro González Labarta <74202468+ralabarta@users.noreply.github.com>
2021-12-28 06:54:50 -0500

    Add files via upload

---
+++ module/base.go
@@ -0,0 +1,9 @@
+package module
+
+import "github.com/loveandpeople-DAG/goClient/trinary"
+
+const (
+	depth                  = 3
+	minimumWeightMagnitude = 5
+	DefaultAddr            = trinary.Trytes("UAHOWGIZS9MFTFFWGVISVMHEESTWEPVZS9CRLPFCZPUNITUMYC9DXOSPDOIZYBQREZFEFGZHPMQWQNBVXUCDTJQ9DB")
+)
